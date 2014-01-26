1b235b6700d0e8cd9ec955bcdc768b6fdb3d1cf0
tkgktyk <tkgktyk@hotmail.co.jp>
2014-01-27 03:57:00 +0900

    update launcher icon. release touch pointer when drag horizontally.

--- FlyingAndroid/src/jp/tkgktyk/flyingandroid/VerticalDragDetectorView.java
+++ FlyingAndroid/src/jp/tkgktyk/flyingandroid/VerticalDragDetectorView.java
@@ -31,11 +31,13 @@ public class VerticalDragDetectorView extends FrameLayout {
 	 * not the same as 'is being flinged', which can be checked by
 	 * mScroller.isFinished() (flinging begins when the user lifts his finger).
 	 */
-	private boolean mIsBeginTouched = false;
+	private boolean mIsBeginTouchedY = false;
+	private boolean mIsBeginTouchedX = false;
 	private boolean mIsBeginDragged = false;
 	/**
 	 * Position of the last motion event.
 	 */
+	private int mLastMotionX;
 	private int mLastMotionY;
 
 	private float mTouchSlopScaleFactor;
@@ -114,14 +116,17 @@ public class VerticalDragDetectorView extends FrameLayout {
 		 * and he is moving his finger. We want to intercept this motion.
 		 */
 		final int action = ev.getAction();
-		if ((action == MotionEvent.ACTION_MOVE) && (mIsBeginTouched)) {
-			return true;
+		if (action == MotionEvent.ACTION_MOVE) {
+			if (mIsBeginTouchedX)
+				return false;
+			if (mIsBeginTouchedY)
+				return true;
 		}
 
 		switch (action & MotionEvent.ACTION_MASK) {
 		case MotionEvent.ACTION_MOVE: {
 			/*
-			 * mIsBeginTouched == false, otherwise the shortcut would have
+			 * mIsBeginTouchedY == false, otherwise the shortcut would have
 			 * caught it. Check whether the user has moved far enough from his
 			 * original down touch.
 			 */
@@ -144,6 +149,14 @@ public class VerticalDragDetectorView extends FrameLayout {
 				break;
 			}
 
+			final int x = (int) ev.getX(pointerIndex);
+			final int xDiff = Math.abs(x - mLastMotionX);
+			if (xDiff > mTouchSlop) {
+				// XposedBridge.log("last y: " + mLastMotionY);
+				// XposedBridge.log("y: " + y);
+				// XposedBridge.log("touch slop: " + mTouchSlop);
+				mIsBeginTouchedX = true;
+			}
 			final int y = (int) ev.getY(pointerIndex);
 			final int yDiff = Math.abs(y - mLastMotionY);
 			if (yDiff > mTouchSlop) {
@@ -154,7 +167,7 @@ public class VerticalDragDetectorView extends FrameLayout {
 				// XposedBridge.log("last y: " + mLastMotionY);
 				// XposedBridge.log("y: " + y);
 				// XposedBridge.log("touch slop: " + mTouchSlop);
-				mIsBeginTouched = true;
+				mIsBeginTouchedY = true;
 			}
 			if (yDiff > mDragSlop) {
 				final ViewParent parent = getParent();
@@ -178,6 +191,7 @@ public class VerticalDragDetectorView extends FrameLayout {
 				 * Remember location of down touch. ACTION_DOWN always refers to
 				 * pointer index 0.
 				 */
+				mLastMotionX = (int) ev.getX();
 				mLastMotionY = (int) ev.getY();
 				mActivePointerId = ev.getPointerId(0);
 			}
@@ -189,7 +203,8 @@ public class VerticalDragDetectorView extends FrameLayout {
 			// always does not intercept
 
 			/* Release the drag */
-			mIsBeginTouched = false;
+			mIsBeginTouchedX = false;
+			mIsBeginTouchedY = false;
 			mIsBeginDragged = false;
 			mActivePointerId = INVALID_POINTER;
 			break;
@@ -201,7 +216,10 @@ public class VerticalDragDetectorView extends FrameLayout {
 		 * The only time we want to intercept motion events is if we are in the
 		 * drag mode.
 		 */
-		return mIsBeginTouched;
+		if (mIsBeginTouchedX)
+			return false;
+
+		return mIsBeginTouchedY;
 	};
 
 	@Override
@@ -218,6 +236,7 @@ public class VerticalDragDetectorView extends FrameLayout {
 			if ((x >= getLeft() && x <= getLeft() + mDetectionWidth)
 					|| (x >= getRight() - mDetectionWidth && x <= getRight())) {
 				// Remember where the motion event started
+				mLastMotionX = (int) ev.getX();
 				mLastMotionY = (int) ev.getY();
 				mActivePointerId = ev.getPointerId(0);
 			}
@@ -232,13 +251,21 @@ public class VerticalDragDetectorView extends FrameLayout {
 				break;
 			}
 
-			final int y = (int) ev.getY(activePointerIndex);
-			int deltaY = mLastMotionY - y;
-			if (!mIsBeginTouched && Math.abs(deltaY) > mTouchSlop) {
+			final int x = (int) ev.getX(activePointerIndex);
+			int deltaX = mLastMotionX - x;
+			if (!isBeginAnything() && Math.abs(deltaX) > mTouchSlop) {
 				// XposedBridge.log("last y: " + mLastMotionY);
 				// XposedBridge.log("y: " + y);
 				// XposedBridge.log("touch slop: " + mTouchSlop);
-				mIsBeginTouched = true;
+				mIsBeginTouchedX = true;
+			}
+			final int y = (int) ev.getY(activePointerIndex);
+			int deltaY = mLastMotionY - y;
+			if (!mIsBeginTouchedY && Math.abs(deltaY) > mTouchSlop) {
+				// XposedBridge.log("last y: " + mLastMotionY);
+				// XposedBridge.log("y: " + y);
+				// XposedBridge.log("touch slop: " + mTouchSlop);
+				mIsBeginTouchedY = true;
 				final ViewParent parent = getParent();
 				if (parent != null) {
 					parent.requestDisallowInterceptTouchEvent(true);
@@ -259,32 +286,40 @@ public class VerticalDragDetectorView extends FrameLayout {
 			break;
 		}
 		case MotionEvent.ACTION_UP: {
-			if (mIsBeginTouched) {
+			if (isBeginAnything()) {
 				mActivePointerId = INVALID_POINTER;
-				mIsBeginTouched = false;
+				mIsBeginTouchedX = false;
+				mIsBeginTouchedY = false;
 				mIsBeginDragged = false;
 			}
 			break;
 		}
 		case MotionEvent.ACTION_CANCEL:
-			if (mIsBeginTouched && getChildCount() > 0) {
+			if (isBeginAnything() && getChildCount() > 0) {
 				mActivePointerId = INVALID_POINTER;
-				mIsBeginTouched = false;
+				mIsBeginTouchedX = false;
+				mIsBeginTouchedY = false;
 				mIsBeginDragged = false;
 			}
 			break;
 		case MotionEvent.ACTION_POINTER_DOWN: {
 			final int index = ev.getActionIndex();
+			mLastMotionX = (int) ev.getX(index);
 			mLastMotionY = (int) ev.getY(index);
 			mActivePointerId = ev.getPointerId(index);
 			break;
 		}
 		case MotionEvent.ACTION_POINTER_UP:
 			onSecondaryPointerUp(ev);
+			mLastMotionX = (int) ev.getX(ev.findPointerIndex(mActivePointerId));
 			mLastMotionY = (int) ev.getY(ev.findPointerIndex(mActivePointerId));
 			break;
 		}
 		return true;
+	}
+
+	private boolean isBeginAnything() {
+		return mIsBeginTouchedX || mIsBeginTouchedY || mIsBeginDragged;
 	}
 
 	private void onSecondaryPointerUp(MotionEvent ev) {
@@ -295,6 +330,7 @@ public class VerticalDragDetectorView extends FrameLayout {
 			// active pointer and adjust accordingly.
 			// TODO: Make this decision more intelligent.
 			final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+			mLastMotionX = (int) ev.getX(newPointerIndex);
 			mLastMotionY = (int) ev.getY(newPointerIndex);
 			mActivePointerId = ev.getPointerId(newPointerIndex);
 		}
