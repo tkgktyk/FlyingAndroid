74adfb966c16cffedc64af13af8fe00f68b80b10
tkgktyk <tkgktyk@hotmail.co.jp>
2014-04-05 00:57:15 +0900

    reload timing of settings is changed from reboot to Activity is recreated. release 0.6.3(17)

--- FlyingAndroid/src/jp/tkgktyk/flyingandroid/FlyingHelper.java
+++ FlyingAndroid/src/jp/tkgktyk/flyingandroid/FlyingHelper.java
@@ -14,7 +14,6 @@ import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 import de.robv.android.xposed.XposedBridge;
 
