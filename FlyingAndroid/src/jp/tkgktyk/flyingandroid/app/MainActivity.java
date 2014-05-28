4658e89851c7deb952306c7849a84d65271077ea
Takagi Katsuyuki <tkgktyk@gmail.com>
2014-05-28 22:52:35 +0900

    Use two-pane preference to arrange settings.

--- FlyingAndroid/src/jp/tkgktyk/flyingandroid/app/MainActivity.java
+++ FlyingAndroid/src/jp/tkgktyk/flyingandroid/app/MainActivity.java
@@ -1,8 +1,9 @@
 package jp.tkgktyk.flyingandroid.app;
 
+import java.util.List;
+
 import jp.tkgktyk.flyingandroid.FA;
 import jp.tkgktyk.flyingandroid.R;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
@@ -15,56 +16,44 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends PreferenceActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		getFragmentManager().beginTransaction()
-				.replace(android.R.id.content, new SettingsFragment()).commit();
+	public void onBuildHeaders(List<Header> target) {
+		loadHeadersFromResource(R.xml.preference_headers, target);
 	}
 
-	public static class SettingsFragment extends PreferenceFragment {
+	@Override
+	protected boolean isValidFragment(String fragmentName) {
+		return FlyingFragment.class.getName().equals(fragmentName)
+				|| PinningFragment.class.getName().equals(fragmentName)
+				|| TroubleshootingFragment.class.getName().equals(fragmentName);
+	}
+
+	@Override
+	public void onHeaderClick(Header header, int position) {
+		super.onHeaderClick(header, position);
+
+		if (header.id == R.id.donate) {
+			PreferenceManager
+					.getDefaultSharedPreferences(this)
+					.edit()
+					.putBoolean(getString(R.string.pref_key_feeling_to_donate),
+							true).apply();
+		}
+	}
+
+	public static class SettingFragment extends PreferenceFragment {
 
 		@SuppressWarnings("deprecation")
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			getPreferenceManager().setSharedPreferencesMode(
 					PreferenceActivity.MODE_WORLD_READABLE);
-			addPreferencesFromResource(R.xml.settings_preference);
-
-			// scroll speed
-			showTextSummary(R.string.pref_key_speed);
-			// initial position
-			openActivity(R.string.pref_key_initial_position,
-					InitialPositionActivity.class);
-			// use notification toggle
-			setupNotificationToggle();
-			// pin position
-			openActivity(R.string.pref_key_pin_position, MovePinActivity.class);
-			// auto pin
-			showListSummary(R.string.pref_key_auto_pin_selection);
-			// white list
-			openSelectorOnClick(R.string.pref_key_white_list,
-					R.string.show_only_white);
-			// niwatori button
-			setupNiwatoriButton();
-			// force set black background
-			openSelectorOnClick(R.string.pref_key_force_set_black_background,
-					R.string.show_only_checked);
-			// black list
-			openSelectorOnClick(R.string.pref_key_black_list,
-					R.string.show_only_black);
 		}
 
 		protected Preference findPreference(int id) {
 			return findPreference(getString(id));
-		}
-
-		private String getSharedString(int keyId) {
-			return PreferenceManager.getDefaultSharedPreferences(getActivity())
-					.getString(getString(keyId), null);
 		}
 
 		private final OnPreferenceChangeListener mListChangeListener = new OnPreferenceChangeListener() {
@@ -76,10 +65,15 @@ public class MainActivity extends Activity {
 			}
 		};
 
-		private void showListSummary(int id) {
+		protected void showListSummary(int id) {
 			ListPreference list = (ListPreference) findPreference(id);
 			list.setOnPreferenceChangeListener(mListChangeListener);
 			setListSummary(list, getSharedString(id));
+		}
+
+		private String getSharedString(int keyId) {
+			return PreferenceManager.getDefaultSharedPreferences(getActivity())
+					.getString(getString(keyId), null);
 		}
 
 		private void setListSummary(ListPreference pref, String value) {
@@ -93,7 +87,7 @@ public class MainActivity extends Activity {
 			pref.setSummary(getString(R.string.current_s1, entry));
 		}
 
-		private void showTextSummary(int id) {
+		protected void showTextSummary(int id) {
 			EditTextPreference et = (EditTextPreference) findPreference(id);
 			et.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 				@Override
@@ -108,7 +102,7 @@ public class MainActivity extends Activity {
 					et.getText());
 		}
 
-		private void openSelectorOnClick(int id, final int textId) {
+		protected void openSelectorOnClick(int id, final int textId) {
 			Preference pref = findPreference(id);
 			pref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
 				@Override
@@ -125,7 +119,7 @@ public class MainActivity extends Activity {
 			});
 		}
 
-		private void openActivity(int id, final Class<?> cls) {
+		protected void openActivity(int id, final Class<?> cls) {
 			Preference pref = findPreference(id);
 			pref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
 				@Override
@@ -135,6 +129,22 @@ public class MainActivity extends Activity {
 					return true;
 				}
 			});
+		}
+	}
+
+	public static class FlyingFragment extends SettingFragment {
+
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_flying);
+
+			// scroll speed
+			showTextSummary(R.string.pref_key_speed);
+			// initial position
+			openActivity(R.string.pref_key_initial_position,
+					InitialPositionActivity.class);
+			// use notification toggle
+			setupNotificationToggle();
 		}
 
 		private void setupNotificationToggle() {
@@ -150,26 +160,47 @@ public class MainActivity extends Activity {
 						}
 					});
 		}
+	}
+
+	public static class PinningFragment extends SettingFragment {
+
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_pinning);
+
+			// pin position
+			openActivity(R.string.pref_key_pin_position, MovePinActivity.class);
+			// auto pin
+			showListSummary(R.string.pref_key_auto_pin_selection);
+			// white list
+			openSelectorOnClick(R.string.pref_key_white_list,
+					R.string.show_only_white);
+			// niwatori button
+			setupNiwatoriButton();
+		}
 
 		private void setupNiwatoriButton() {
+			// unlock
 			if (getPreferenceManager().getSharedPreferences().getBoolean(
 					getString(R.string.pref_key_feeling_to_donate), false)) {
 				Preference niwatori = findPreference(R.string.pref_key_use_niwatori_button);
 				niwatori.setEnabled(true);
 			}
-			Preference donate = findPreference(R.string.pref_key_feeling_to_donate);
-			donate.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-				@Override
-				public boolean onPreferenceClick(Preference preference) {
-					getPreferenceManager()
-							.getSharedPreferences()
-							.edit()
-							.putBoolean(
-									getString(R.string.pref_key_feeling_to_donate),
-									true).apply();
-					return false;
-				}
-			});
+		}
+	}
+
+	public static class TroubleshootingFragment extends SettingFragment {
+
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_troubleshooting);
+
+			// force set black background
+			openSelectorOnClick(R.string.pref_key_force_set_black_background,
+					R.string.show_only_checked);
+			// black list
+			openSelectorOnClick(R.string.pref_key_black_list,
+					R.string.show_only_black);
 		}
 	}
 }
